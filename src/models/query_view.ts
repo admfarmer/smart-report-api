b7fbac07fc9e60797c2ed3c1a4e44cd6e64807c8
thawatchai <thawatchai.sea2@gmail.com>
2020-11-13 10:09:01 +0700

    connect

--- src/models/query_view.ts
+++ src/models/query_view.ts
@@ -4,9 +4,8 @@ import knex = require('knex');
 export class QueryViewsModel {
 
   async viewReport(dbHIS: knex, query: string, params: any) {
-    let sql = `set @datestart = ?; set @dateend =? ;  set @icd10 = ?; SELECT icd10,count(o.vn) as total from ovst as o inner join ovstdx as x on o.vn=x.vn where date(o.vstdttm) between @datestart and @dateend and x.icd10 = @icd10`;
 
-    // let sql = query;
+    let sql = query;
     return await dbHIS.raw(sql, params)
   }
 
