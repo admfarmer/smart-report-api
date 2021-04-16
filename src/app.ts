8632614656d2672d5745254a1740da66ea86a9a4
Thawatchai Saengduan <thawatchai.sea2@gmail.com>
2021-04-16 11:18:07 +0700

    get ipd Co ward

--- src/app.ts
+++ src/app.ts
@@ -102,7 +102,7 @@ app.register(require('./plugins/db'), {
       }
     },
     debug: false,
-    acquireConnectionTimeout: 1500000000000,
+    acquireConnectionTimeout: 150000,
   },
   connectionName: 'dbCO'
 });
