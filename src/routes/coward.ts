8974df69f00342a7ae7ca526b8f14ab507cc7e9f
Thawatchai Saengduan <thawatchai.sea2@gmail.com>
2021-04-19 11:49:33 +0700

    get ipd Co ward

--- src/routes/coward.ts
+++ src/routes/coward.ts
@@ -21,30 +21,36 @@ const router = (fastify, { }, next) => {
 
   fastify.get('/info', async (req: fastify.Request, reply: fastify.Reply) => {
     // try {
-      const rs: any = await viewsAdmitModel.viewCoWard(dbHIS);
-      let info = rs[0];
-      let _info = [];
-      let rs_info: any;
-      let _rs_info = [];
-      if(info){
-        info.forEach(async (v: any) => {
-            console.log(v);
-            rs_info = await admissionModels.insert(dbCO,v);
-            console.log(rs_info);
-            _rs_info.push(rs_info[0])
-            _info.push(v);
-        })
-        if(_rs_info[0]){
-            reply.status(HttpStatus.OK).send({ statusCode: HttpStatus.OK, info: _info,rs_info: rs_info });
-        }else{
-            reply.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) })           
-        }
-      }
+      let dbco_type = process.env.DBCO_TYPE || '0';
 
-    // } catch (error) {
-    //   fastify.log.error(error);
-    //   reply.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) })
-    // }
+      if(dbco_type == '1'){
+          const rs: any = await viewsAdmitModel.viewCoWard(dbHIS);
+          let info = rs[0];
+          let _info = [];
+          let rs_info: any;
+          let _rs_info = [];
+      
+          if(info){
+            info.forEach(async (v: any) => {
+                console.log(v);
+              try {
+                      rs_info = await admissionModels.insert(dbCO,v);
+                  } catch (error) {
+                  fastify.log.error(error);
+                  // reply.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) })
+                  }
+                console.log(rs_info);
+                _rs_info.push(rs_info[0])
+                _info.push(v);
+      
+            })
+            reply.status(HttpStatus.OK).send({ statusCode: HttpStatus.OK, info: _info,rs_info: rs_info });
+      
+          }
+      }else{
+          console.log('running Not');
+          
+      }
 
   });
 
